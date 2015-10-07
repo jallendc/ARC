953c70d2b46435664ae4b7af92cd01306de24361
jallendc <lallen16@gmu.edu>
2015-10-07 13:59:36 -0400

    changes for new controller

--- app/src/main/java/com/lofarolabs/udpsender/MainSend.java
+++ app/src/main/java/com/lofarolabs/udpsender/MainSend.java
@@ -1,5 +1,6 @@
 package com.lofarolabs.udpsender;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -191,6 +192,11 @@ public class MainSend extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.diff_cntrl) {
+            startActivity(new Intent(MainSend.this, DiffDriveCntrl.class));
             return true;
         }
 
